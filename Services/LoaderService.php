dfd4b1d68507d127d9062018e6987d37a824159a
Maciej Zakrzewski <maciej.zakrzewski87@gmail.com>
2016-09-02 16:51:34 +0200

    loader additions

--- Services/LoaderService.php
+++ Services/LoaderService.php
@@ -21,7 +21,57 @@ class LoaderService
             throw new JSONParseException($json);
         }
 
+        if(isset($data->namespace)) {
+           $data->namespace = str_replace(":", "\\", $data->namespace);
+        } else {
+            $data->namespace = null;
+        }
+
         return $data;
     }
+
+    public function loadJsonFile($file)
+    {
+        $json = file_get_contents($file);
+
+        return $this->loadJsonData($json);
+    }
+
+
+    public function loadEntityFromJson($json)
+    {
+        $data = $this->loadJsonData($json);
+
+        $nsPrefix = '';
+
+        if(!empty($data->namespace)) {
+            $nsPrefix = $data->namespace.'\\';
+        }
+
+        $entityClassName = $nsPrefix.$data->entityName;
+
+        //http://php.net/manual/pl/class.reflectionclass.php
+        //http://php.net/manual/pl/reflectionproperty.setvalue.php
+        //http://stackoverflow.com/questions/6448551/is-there-any-way-to-set-a-private-protected-static-property-using-reflection-cla
+
+        $refClass = new \ReflectionClass($entityClassName);
+
+        var_dump($refClass);
+
+        foreach($data->entries as $entry) {
+            $entryArr = (array)$entry;
+            $entity = new $entityClassName(); //TODO: make it work with parametered constructors
+            foreach($entryArr as $key=>$val) {
+                $property = $refClass->getProperty($key);
+                $property->setValue($entity, $property);
+            }
+
+            var_dump($entry);
+        }
+        
+    }
+
+    
+
 }
 
--- Tests/LoaderTest.php
+++ Tests/LoaderTest.php
@@ -15,18 +15,30 @@ class LoaderTest extends BaseTestCase
 
     public function testLoaderParsing()
     {
-
         $json = file_get_contents(__DIR__.'/json/TestEntitySimpleReplace.json');
         $data = $this->loader->loadJsonData($json);
-      
+        $this->assertEquals($data->namespace, 'MZ314\JSonFixturesBundle\Tests\Entity');
         $this->assertEquals($data->entityName, 'TestEntity');
         $this->assertEquals($data->mode, 'replace');
         $this->assertEquals(count($data->entries), 2);
         $this->assertTrue(is_array($data->entries));
         $a =["id"=>0, "name"=> "test1"];
         $b = ["id"=>1, "name"=> "test2"];
-        $this->assertEquals($data->entries, [(object)$a, (object)$b]);
+        $this->assertEquals($data->entries, [(object)$a, (object)$b]);   
+    }
+
+    public function testLoaderFileLoading()
+    {
+        $data = $this->loader->loadJsonFile(__DIR__.'/json/TestEntitySimpleReplace.json');
+        $this->assertFalse(is_null($data));
+    }
+
+    public function testLoadingEntity()
+    {
+        $json = file_get_contents(__DIR__.'/json/TestEntitySimpleReplace.json');
         
+
+        $this->loader->loadEntityFromJson($json);
     }
 }
 
