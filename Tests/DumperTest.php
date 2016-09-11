137fa99a9ad0d431d16cab3fca58b4f81954e6a7
Maciej Zakrzewski <maciej.zakrzewski87@gmail.com>
2016-09-11 18:55:35 +0200

    dumper test coverage

--- Tests/DumperTest.php
+++ Tests/DumperTest.php
@@ -19,13 +19,19 @@ class DumperTest extends BaseTestCase
 
     public function testDump()
     {
-
-
+        
         $this->loader->loadFromJson(file_get_contents(__DIR__.'/json/TestEntitySimpleReplace.json'));
         $this->em->flush();
-        
         $json = $this->dumper->dumpToJson($this->em->getRepository('JSonFixturesBundle:TestEntity')->findAll());
-        var_dump($json);
         
+        $data = json_decode($json);
+        
+        $this->assertEquals($data->entityName, 'TestEntity');
+        $this->assertEquals($data->namespace, 'MZ314:JSonFixturesBundle:Tests:Entity');
+        $this->assertEquals(count($data->entries), 2);
+        $this->assertEquals($data->entries[0]->id, 6);
+        $this->assertEquals($data->entries[0]->name, 'test1');
+        $this->assertEquals($data->entries[1]->id, 7);
+        $this->assertEquals($data->entries[1]->name, 'test2');
     }
 }
