75585800d8fc3c4835ab08f47e341d9eac7f0330
Maciej Zakrzewski <maciej.zakrzewski87@gmail.com>
2016-09-11 18:50:38 +0200

    some dumper additions

--- Services/DumperService.php
+++ Services/DumperService.php
@@ -2,8 +2,10 @@
 
 namespace MZ314\JSonFixturesBundle\Services;
 
+
 class DumperService
 {
+
     protected $em;
 
     public function __construct($em)
@@ -11,10 +13,38 @@ class DumperService
         $this->em = $em;
     }
 
+    protected function entityTostdClass($entity, \ReflectionClass $reflection)
+    {
+        $class = new \stdClass();
+        
+        $properties = $reflection->getProperties();
+        
+        foreach($properties as $property) {
+            $property->setAccessible(true);
+            $pName = $property->getName();
+            $class->$pName = $property->getValue($entity);
+        }
+        
+        return $class;
+        
+    }
+    
     public function dumpToJson(Array $entities)
     {
-        $json = '';
+        $data = new \stdClass();
+        
+        $reflection = new \ReflectionClass(get_class($entities[0]));
 
+        $data->entityName = $reflection->getShortName();
+        $data->namespace = str_replace('\\', ':', $reflection->getNamespaceName());
+        $data->entries = [];
+
+        foreach ($entities as $entity) {
+                $data->entries[] = $this->entityTostdClass($entity, $reflection);
+        }
+
+        $json = json_encode($data);
+        
         return $json;
     }
 }
